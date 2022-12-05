0b748d404f526f5ca2d47a66755a793047751c48
Getabicha Daniel <getabicha.weldekidan@gmail.com>
2022-12-05 15:01:55 +0200

    Rect pro.

---
+++ expat-blog/src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Expat Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Insights</a>
+
+            </div>
+
+        </nav>
+        
+     );
+}
+ 
+export default Navbar;
